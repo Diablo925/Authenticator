8fdca25ac46f8330a7868432612ba8aae7ae3d64
Carsten Andersen <carsten@alio.dk>
2015-08-23 13:10:06 +0200

    small update

--- code/controller.ext.php
+++ code/controller.ext.php
@@ -1,8 +1,8 @@
 <?php
 class module_controller extends ctrl_module
 {
-		
-		
+
+
     /**
      * The 'worker' methods.
      */
@@ -22,25 +22,25 @@ class module_controller extends ctrl_module
 			return true;
 				return false;
     }
-	
+
 	static function ExecuteUpdate($uid, $enable)
 	 {
 		 global $zdbh;
-		 global $controller;		 
+		 global $controller;
 		$currentuser = ctrl_users::GetUserDetail();
 		include('GoogleAuthenticator.php');
 		$ga = new PHPGangsta_GoogleAuthenticator();
-		
-		
+
+
 		$sql = $zdbh->prepare("SELECT ac_otp_vc FROM x_accounts WHERE ac_id_pk = :uid AND ac_deleted_ts IS NULL");
 		$sql->bindParam(':uid', $currentuser['userid']);
 		$sql->execute();
 		$result = $sql->fetch();
 		$status = $result['ac_otp_vc'];
-		
+
 		if($enable == 1)
 		{
-			if($status == 0) { 
+			if($status == 0) {
 		$secret = $ga->_createSecret();
 		$sql = $zdbh->prepare("UPDATE x_accounts SET ac_otp_vc=:enable, ac_otpkey_vc=:Seckey WHERE ac_id_pk = :uid");
 		$sql->bindParam(':enable', $enable);
@@ -49,9 +49,9 @@ class module_controller extends ctrl_module
         $sql->execute();
 			}
 			return true;
-		
+
 		}
-		if($enable == 0) 
+		if($enable == 0)
 		{
 		$otp_key = "";
 		$sql = $zdbh->prepare("UPDATE x_accounts SET ac_otp_vc=:enable, ac_otpkey_vc=:Seckey WHERE ac_id_pk = :uid");
@@ -59,12 +59,12 @@ class module_controller extends ctrl_module
 		$sql->bindParam(':Seckey', $otp_key);
         $sql->bindParam(':uid', $currentuser['userid']);
         $sql->execute();
-			
+
 			return true;
 		}
-		
+
 			return true;
-		
+
 	 }
 	/**
      * End 'worker' methods.
@@ -72,14 +72,14 @@ class module_controller extends ctrl_module
 
     /**
      * Webinterface sudo methods.
-     */ 
-	 		
+     */
+
 	static function getBarcode() {
 		global $zdbh;
 		global $controller;
 		require_once 'GoogleAuthenticator.php';
 		$ga = new PHPGangsta_GoogleAuthenticator();
-		
+
 	$currentuser = ctrl_users::GetUserDetail();
 	$sql = $zdbh->prepare("SELECT ac_otpkey_vc FROM x_accounts WHERE ac_id_pk = :uid AND ac_deleted_ts IS NULL");
 	$sql->bindParam(':uid', $currentuser['userid']);
@@ -89,12 +89,12 @@ class module_controller extends ctrl_module
 	$website = ctrl_options::GetSystemOption('sentora_domain');
 	$barcode = $ga->getQRCodeGoogleUrl($currentuser['username'], $secret, $website);
 	$barimg = "<image src=". $barcode .">";
-	
+
 		if(!empty($secret) || ($secret == 'null')) {
         return $barimg;
 		} else { return ""; }
     }
-	
+
 	static function getEnable() {
 		global $zdbh;
 		global $controller;
@@ -105,11 +105,11 @@ class module_controller extends ctrl_module
 	$result = $sql->fetch();
 	$secret = $result['ac_otp_vc'];
 		if($secret == 1) {
-        return "CHECKED"; } else { 
+        return "CHECKED"; } else {
 		return ""; }
     }
-	
-	 
+
+
 	static function getSecKey() {
 		global $zdbh;
 		global $controller;
@@ -119,18 +119,18 @@ class module_controller extends ctrl_module
     $sql->execute();
 	$result = $sql->fetch();
 	$secret = $result['ac_otpkey_vc'];
-	
+
 		return $secret;
     }
-	
+
 	static function getZadmininfo() {
 		global $controller;
 	$currentuser = ctrl_users::GetUserDetail();
-	if($currentuser['username'] == "zadmin") { 
+	if($currentuser['username'] == "zadmin") {
 	$res = "You have to download the additional <a href=http://repo.alio.dk/auth/more.zip targer=_blank>file</a> to get it to work.<br />
  they must be copied into their raspative directory under /etc/sentora/panel/...<br />
- <b>Otherwise it does not.</b>"; } else { $res = ""; }
-	
+ <b>Otherwise it does not work.</b>"; } else { $res = ""; }
+
 		return $res;
     }
 	 /**
